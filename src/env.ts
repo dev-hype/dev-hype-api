9595a108b51b9600b2972ad40afd348664c411bb
“AdhamMoussa” <adam.moussa62017@gmail.com>
2022-07-06 23:51:27 +0700

    env config

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import * as Joi from 'joi'
+
+export const envSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+  FRONTEND_URL: Joi.string().required(),
+
+  // Database
+  DATABASE_URL: Joi.string().required(),
+
+  // Auth
+  // JWT_SECRET: Joi.string().required(),
+  // GITHUB_AUTH_CLIENT_ID: Joi.string().required(),
+  // GITHUB_AUTH_CLIENT_SECRET: Joi.string().required(),
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  // config
+  const configService = app.get(ConfigService)
+  const PORT = configService.get('PORT')
+
+  app.enableCors({
+    origin: [configService.get('FRONTEND_URL')],
+  })
+
+  await app.listen(PORT)
 }
-bootstrap();
+
+bootstrap()
